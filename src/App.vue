fa394c08d2fe064d1bf33c22c270f47d9be8742c
贺齐龙 <hql01194210@alibaba-inc.com>
2022-01-20 16:40:03 +0800

    将进酒

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,27 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <h1>新年快乐</h1>
     <h2>新年快乐🎆</h2>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <li>君不见，黄河之水天上来，奔流到海不复回。</li>
+      <li>君不见，高堂明镜悲白发，朝如青丝暮成雪。</li>
+      <li>人生得意须尽欢，莫使金樽空对月。</li>
+      <li>天生我材必有用，千金散尽还复来。</li>
+      <li>烹羊宰牛且为乐，会须一饮三百杯。</li>
+      <li>岑夫子，丹丘生，将进酒，君莫停。</li>
+      <li>与君歌一曲，请君为我侧耳听。</li>
+      <li>钟鼓馔玉不足贵，但愿长醉不复醒。</li>
+      <li>古来圣贤皆寂寞，惟有饮者留其名。</li>
+      <li>陈王昔时宴平乐，斗酒十千恣欢谑。</li>
+      <li>主人何为言少钱，径须沽取对君酌。</li>
+      <li>五花马，千金裘，</li>
+      <li>呼儿将出换美酒，与尔同销万古愁。</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
@@ -29,5 +38,13 @@ export default {
 }
 h1 {
   color: tomato;
+}
+ul {
+  padding: 0;
+  margin: 0;
+}
+ul li {
+  list-style: none;
+  font-size: 20px;
 }
 </style>
