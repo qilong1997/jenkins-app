984c9f7260ca4a2ade41708630acf3aab7a78a55
贺齐龙 <hql01194210@alibaba-inc.com>
2022-01-20 16:14:58 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <h1>新年快乐</h1>
+    <h2>新年快乐🎆</h2>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
